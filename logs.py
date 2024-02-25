2c2641520063549b29bc6702ffdfa3485fe4b4d8
Phill <phillipwood01@gmail.com>
2024-02-25 11:19:36 +0000

    omg im gonna kms

--- logs.py
+++ logs.py
@@ -7,32 +7,6 @@ logs = len(os.listdir(logpath))
 if not os.path.exists(logpath):
     os.makedirs(logpath)
 
-logging.basicConfig(filename=f'{logpath}\{logs+1} {date.today()} Log.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
-
-
-def info(message):
-    strmessage = str(message)
-    logging.info(strmessage)
-    print(strmessage)
-
-def warn(message):
-    strmessage = str(message)
-    logging.warn(strmessage)
-    print(strmessage)
-
-def error(exc):
-    logging.error("Something Went Wrong!", exc_info=exc)
-    print("Something Went Wrong! Check Logs for information")
-=======
-import logging
-import os
-from datetime import date
-logpath = os.path.join(os.path.dirname(__file__), "Logs")
-logs = len(os.listdir(logpath))
-
-if not os.path.exists(logpath):
-    os.makedirs(logpath)
-
 logging.basicConfig(filename=f'{logpath}\\{logs+1} {date.today()} Log.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
 
 
@@ -49,3 +23,4 @@ def warn(message):
 def error(exc):
     logging.error("Something Went Wrong!", exc_info=exc)
     print("Something Went Wrong! Check Logs for information")
+
